fecdacc5ff0bf5773adec78853ff0ad1e3ce68dc
Matt McCarley <mdmccarley89@gmail.com>
2021-03-02 15:03:01 -0800

    first push

---
+++ index.js
@@ -0,0 +1,19 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 9000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/*", (_, res) => {
+    res.json({
+        data: "The API LIVES!!!"
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is alive on port ${port}`);
+})
